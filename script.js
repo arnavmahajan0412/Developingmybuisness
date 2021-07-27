8ba3d2f0f8d7345fed208534c995353f20cf5d1e
arnavmahajan0412 <arnavmahajan0412@gmail.com>
2021-07-27 10:11:35 +0530

    new commit

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/*$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});*/
 $(document).ready(function() {
     $(".fancybox").fancybox({
         openEffect: "none",
